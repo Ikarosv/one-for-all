05f520537505999bcd1300add41f83f65b886a13
Ikarosv <ikarovieiraa@gmail.com>
2023-03-06 16:11:18 -0300

    fix seleciona o DB

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,8 +1,8 @@
-SELECT nome_pessoa_usuaria,
+SELECT usuario.nome_pessoa_usuaria as pessoa_usuaria,
 	COUNT(historico.cancao) AS musicas_ouvidas,
-    ROUND(SUM(duracao) / 60, 2) AS total_minutos
-FROM usuario
-JOIN historico ON usuario.pessoa_usuaria_id = historico.fk_pessoa_usuaria_id
-JOIN cancoes on historico.cancao = cancoes.cancao
+    ROUND(SUM(cancoes.duracao) / 60, 2) AS total_minutos
+FROM SpotifyClone.usuario
+JOIN SpotifyClone.historico ON usuario.pessoa_usuaria_id = historico.fk_pessoa_usuaria_id
+JOIN SpotifyClone.cancoes on historico.cancao = cancoes.cancao
 GROUP BY nome_pessoa_usuaria
 ORDER BY nome_pessoa_usuaria;
