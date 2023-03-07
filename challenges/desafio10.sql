d5e24dae184b4a00ff1dbe14116f848483cfb327
Ikarosv <ikarovieiraa@gmail.com>
2023-03-06 21:13:02 -0300

    feat: cria tabela canções favoritas

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,26 @@
--- Descomente e altere as linhas abaixo:
+USE SpotifyClone;
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
+CREATE TABLE IF NOT EXISTS cancoes_favoritas(
+	pessoa_usuaria_id INT NOT NULL,
+	cancao_favorita VARCHAR(45) NOT NULL,
+    FOREIGN KEY (pessoa_usuaria_id) REFERENCES usuario(pessoa_usuaria_id),
+    FOREIGN KEY (cancao_favorita) REFERENCES cancoes(cancao),
+    PRIMARY KEY (cancao_favorita, pessoa_usuaria_id)
+) engine = InnoDB;
 
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO SpotifyClone.cancoes_favoritas (pessoa_usuaria_id, cancao_favorita)
+VALUES
+  (1, 'ALIEN SUPERSTAR'),
+  (1, 'Como Nossos Pais'),
+  (1, 'Feeling Good'),
+  (2, "Don't Stop Me Now"),
+  (3, 'BREAK MY SOUL'),
+  (3, 'ALIEN SUPERSTAR'),
+  (4, 'O Medo de Amar é o Medo de Ser Livre'),
+  (4, "Don't Stop Me Now"),
+  (5, 'Feeling Good'),
+  (5, "VIRGO'S GROOVE"),
+  (6, "Don't Stop Me Now"),
+  (7, "O Medo de Amar é o Medo de Ser Livre"),
+  (8, "ALIEN SUPERSTAR")
+  ;
